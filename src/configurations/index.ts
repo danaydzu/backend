b0d46322d486b6a2206625ba4b2b16926194b54d
danaydzu <yoyebears@gmail.com>
2023-06-28 20:56:38 +0300

    backend

--- src/configurations/index.ts
+++ src/configurations/index.ts
@@ -1,7 +1,13 @@
 import * as process from "process";
 
 export default () => ({
-    port: process.env.PORT
 
+    port: process.env.PORT,
+
+    db_port: process.env.DB_PORT,
+    db_name: process.env.DB_NAME,
+    db_user: process.env.DB_USER,
+    db_password: process.env.DB_PASSWORD,
+    db_host: process.env.DB_HOST
 
 })
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,8 +2,9 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {UserModule} from '../user/user.module';
-import {ConfigModule} from "@nestjs/config";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 import configurations from "../../configurations";
+import {SequelizeModule} from "@nestjs/sequelize";
 
 @Module({
     imports: [
@@ -11,7 +12,28 @@ import configurations from "../../configurations";
         ConfigModule.forRoot({
             isGlobal: true,
             load: [configurations]
-        })],
+        }),
+        SequelizeModule.forRootAsync({
+            imports: [
+                ConfigModule,
+            ],
+            inject: [
+                ConfigService
+            ],
+            useFactory: (configService: ConfigService) => ({
+                dialect: "postgres",
+                host: configService.get('db_host'),
+                port: configService.get('db_port'),
+                password: configService.get('db_password'),
+                username: configService.get('db_user'),
+                database: configService.get('db_name'),
+                synchronize: true,
+                autoLoadModels: true,
+                models: []
+            })
+        })
+    ],
+
     controllers: [AppController],
     providers: [AppService],
 
